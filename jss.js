417a0db796ab3db076e07a2ed65c57019a737e5e
ajayrai03 <ajayrai1012@gmail.com>
2022-02-08 23:20:34 +0530

    backtick

---
+++ jss.js
@@ -0,0 +1,26 @@
+var a;
+a=10;
+console.log(a);
+console.log(typeof a);
+
+a="hello";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=10.01;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+var str='ajay';
+console.log(str);
+console.log(typeof str);
+// backtick
+var num=5000;
+console.log(`half of ${num} is ${num/2}`);
