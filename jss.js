d70c032a6cf700a530520113e312620129e892cf
ajayrai03 <ajayrai1012@gmail.com>
2022-02-09 21:00:04 +0530

    for loop use here

--- jss.js
+++ jss.js
@@ -1,40 +1,48 @@
-var a;
-a=10;
-console.log(a);
-console.log(typeof a);
+// var a;
+// a=10;
+// console.log(a);
+// console.log(typeof a);
 
-a="hello";
-console.log(a);
-console.log(typeof a);
+// a="hello";
+// console.log(a);
+// console.log(typeof a);
 
-a=true;
-console.log(a);
-console.log(typeof a);
+// a=true;
+// console.log(a);
+// console.log(typeof a);
 
-a=10.01;
-console.log(a);
-console.log(typeof a);
+// a=10.01;
+// console.log(a);
+// console.log(typeof a);
 
-a=null;
-console.log(a);
-console.log(typeof a);
-var str='ajay';
-console.log(str);
-console.log(typeof str);
-// backtick
-var num=5000;
-console.log(`half of ${num} is ${num/2}`);
-var h='hey';
-console.log(h);
- var a=50;
-console.log(a);
-var a=100;
-console.log(a);
-let a=70;
-console.log(a);
-a=90;
-console.log(a);
-const a=45;
-console.log(a);
-const b=75;
-console.log(b);
+// a=null;
+// console.log(a);
+// console.log(typeof a);
+// var str='ajay';
+// console.log(str);
+// console.log(typeof str);
+// // backtick
+// var num=5000;
+// console.log(`half of ${num} is ${num/2}`);
+// var h='hey';
+// console.log(h);
+//  var a=50;
+// console.log(a);
+// var a=100;
+// console.log(a);
+// let a=70;
+// console.log(a);
+// a=90;
+// console.log(a);
+// const a=45;
+// console.log(a);
+// const b=75;
+// console.log(b);
+var n=10;
+for(var i=0;i<n;i++)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+}
