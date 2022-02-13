677f648341db71ef8d2079541fda1708ea532c70
ajayrai03 <ajayrai1012@gmail.com>
2022-02-13 22:14:13 +0530

    1darray

--- jss.js
+++ jss.js
@@ -56,7 +56,9 @@
 // }
 // console.log(i);
 // console.log(j);
-let arr2d=[[1,2,3], [4,5,6], [7,8,9]];
-console.table(arr2d);
-console.log(arr2d[1][2]);
-console.log(arr2d.length);
+// let arr2d=[[1,2,3], [4,5,6], [7,8,9]];
+// console.table(arr2d);
+// console.log(arr2d[1][2]);
+// console.log(arr2d.length);
+let arr1d=["bmw", "tata", 1,4,0,0,3];
+console.log(arr1d.length);
