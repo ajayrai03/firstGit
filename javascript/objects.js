f6968f94132cf83f6b2dba0938d1689576335cda
ajayrai03 <ajayrai1012@gmail.com>
2022-02-23 14:13:45 +0530

    node objects

--- javascript/objects.js
+++ javascript/objects.js
@@ -6,3 +6,20 @@ let car={
     };
     console.log(car);
     console.log(car.available_models[0]);
+let elon_musk={
+space_company:"spacex",
+car_comapany:"tesla",
+solar_panel:"solarroof",
+wives:{
+    first_wife:"talulah riley",
+    second_wife:"justine musk",
+    },
+    ambitious:"to settle in mars",
+    sayhi: function(){
+        console.log(`hello elon musk is famous for his space and car company --> ${this.space_company} ${this.car_comapany}`);
+    }
+};
+console.log(elon_musk);
+console.log(elon_musk.car_comapany);
+console.log(elon_musk.wives["second_wife"]);
+elon_musk.sayhi();
